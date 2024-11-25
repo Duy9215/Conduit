7ee2abd1b7eb5756e0e660a65bd5d3d92811e7f7
DuongVietDuy <duydv.forwork@gmail.com>
2024-11-26 02:34:32 +0700

    default

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,43 +1,40 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from '../assets/styles/Contents.module.css';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../assets/styles/Contents.module.css";
 
 const Contents = () => {
-    const [selectedComponent, setSelectedComponent] = useState('Blog');
+  const [selectedComponent, setSelectedComponent] = useState("Blog");
 
-    const handleLinkClick = (component) => {
-        setSelectedComponent(component);
-    };
+  const handleLinkClick = (component) => {
+    setSelectedComponent(component);
+  };
 
-    return (
-        <div className={styles.content}>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink
-                            className={`${selectedComponent === 'Blog' ? styles.active : ''
-                                }`}
-                            onClick={() => handleLinkClick('Blog')}
-                        >
-                            Blogs
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            className={`${selectedComponent === 'BlogYourFeed' ? styles.active : ''
-                                }`}
-                            onClick={() => handleLinkClick('BlogYourFeed')}
-                        >
-                            Your Feed
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className={styles.content}>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              className={`${selectedComponent === "Blog" ? styles.active : ""}`}
+              onClick={() => handleLinkClick("Blog")}
+            >
+              Blogs
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={`${
+                selectedComponent === "BlogYourFeed" ? styles.active : ""
+              }`}
+              onClick={() => handleLinkClick("BlogYourFeed")}
+            >
+              Your Feed
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Contents;
-
-
-
