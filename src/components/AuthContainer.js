e7634dcfc72662dd192a1b5d5883ff21d4b99927
DuongVietDuy <duydv.forwork@gmail.com>
2024-12-03 00:51:12 +0700

    them route blogdetail

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePages from '../pages/HomePages';
-import Login from '../pages/Login';
+import BlogDetails from './BlogDetails';
 
 const AuthContainer = () => {
     return (
@@ -9,6 +11,8 @@ const AuthContainer = () => {
             <Routes>
                 <Route path="/" element={<HomePages />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/articles/:slug" element={<BlogDetails />} />
             </Routes>
         </Router>
     );
