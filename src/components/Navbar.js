1092f189f253257beaa4fdf3d9bd87e10cac4deb
DuongVietDuy <duydv.forwork@gmail.com>
2024-12-03 19:48:10 +0700

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,14 +22,16 @@ const Navbar = ({ isProfile = false }) => {
     navigate("/");
   };
 
-
-
   useEffect(() => {
     const userInfo = localStorage.getItem("user-info");
     if (userInfo) {
       const parsedUserInfo = JSON.parse(userInfo);
-      setUsername(parsedUserInfo.username);
-      setIsLoggedIn(true);
+      if (parsedUserInfo.username) {
+        setUsername(parsedUserInfo.username);
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
     } else {
       setIsLoggedIn(false);
     }
@@ -50,67 +52,84 @@ const Navbar = ({ isProfile = false }) => {
         <div className={styled.Nav}>
           <ul>
             {isLoggedIn ? (
-              <>
+              <div className="d-flex">
+                <Nav>
+                  <Nav.Link
+                    as={Link}
+                    to="/"
+                    className={`${styled.NavLink} ${
+                      location.pathname === "/" ? styled.Active : ""
+                    }`}
+                  >
+                    Home
+                  </Nav.Link>
+                </Nav>
                 {[""].map((variant) => (
                   <DropdownButton
                     as={ButtonGroup}
                     title={`Xin chào, ${username}`}
-                    id={`dropdown-variants-${variant}`}
-                    variant={variant.toLowerCase()}
+                    id="dropdown-variants"
                     show={dropdownShow}
-                    onMouseEnter={() => setDropdownShow(true)}
-                    onMouseLeave={() => setDropdownShow(false)}
-                    className={`${styled.CustomDropdownButton} custom-dropdown`}
+                    onClick={() => setDropdownShow(!dropdownShow)}
+                    className="me-3"
                   >
-                    <Dropdown.Item eventKey="1">
-                      <AiOutlineUser className="mx-2 mb-1" />{" "}
+                    <Dropdown.Item eventKey="1" className="DropdownMenu">
+                      <AiOutlineUser className="mx-2 mb-1" />
                       <Link
-                        to={"/yourprofile"}
+                        to="/yourprofile"
                         style={{ textDecoration: "none", color: "black" }}
                       >
                         Profile
-                      </Link>{" "}
+                      </Link>
                     </Dropdown.Item>
-                    <Dropdown.Item eventKey="1">
-                      <AiOutlineEdit className="mx-2 mb-1" />{" "}
+                    <Dropdown.Item eventKey="1" className="DropdownMenu">
+                      <AiOutlineEdit className="mx-2 mb-1" />
                       <Link
-                        to={"/edit"}
+                        to="/edit"
                         style={{ textDecoration: "none", color: "black" }}
                       >
                         New Article
-                      </Link>{" "}
+                      </Link>
                     </Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout} eventKey="2">
+                    <Dropdown.Divider />
+                    <Dropdown.Item
+                      onClick={handleLogout}
+                      eventKey="2"
+                      className="DropdownMenu"
+                    >
                       <BiLogOut className="mx-2 mb-1" />
-                      Logout
+                      <span>Logout</span>
                     </Dropdown.Item>
                   </DropdownButton>
                 ))}
-              </>
+              </div>
             ) : (
               <li>
                 <Nav>
                   <Nav.Link
                     as={Link}
                     to="/"
-                    className={`${styled.NavLink} ${location.pathname === "/" ? styled.Active : ""
-                      }`}
+                    className={`${styled.NavLink} ${
+                      location.pathname === "/" ? styled.Active : ""
+                    }`}
                   >
                     Home
                   </Nav.Link>
                   <Nav.Link
                     as={Link}
                     to="/login"
-                    className={`${styled.NavLink} ${location.pathname === "/login" ? styled.Active : ""
-                      }`}
+                    className={`${styled.NavLink} ${
+                      location.pathname === "/login" ? styled.Active : ""
+                    }`}
                   >
                     Login
                   </Nav.Link>
                   <Nav.Link
                     as={Link}
                     to="/register"
-                    className={`${styled.NavLink} ${location.pathname === "/register" ? styled.Active : ""
-                      }`}
+                    className={`${styled.NavLink} ${
+                      location.pathname === "/register" ? styled.Active : ""
+                    }`}
                   >
                     Register
                   </Nav.Link>
