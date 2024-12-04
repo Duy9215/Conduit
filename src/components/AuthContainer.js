fe2ccef74661d175a58c4f585b9efd962038ccae
DuongVietDuy <duydv.forwork@gmail.com>
2024-12-04 21:20:41 +0700

    mock2-final-update

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -3,7 +3,7 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePages from '../pages/HomePages';
-import Editor from '../pages/Editor';
+import AddAriticle from '../pages/AddAriticle';
 import BlogDetails from './BlogDetails';
 import Profile from './Profile';
 
@@ -14,8 +14,8 @@ const AuthContainer = () => {
                 <Route path="/" element={<HomePages />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/edit" element={< Editor />} />
-                <Route path="/profiles/:username" element={< Profile />} />
+                <Route path="/add" element={<AddAriticle />} />
+                <Route path="/profiles/:username" element={<Profile />} />
                 <Route path="/articles/:slug" element={<BlogDetails />} />
             </Routes>
         </Router>
