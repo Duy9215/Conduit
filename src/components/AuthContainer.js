fdb156be872408f6ea752463b2db059f3e6229e5
DuongVietDuy <duydv.forwork@gmail.com>
2024-12-09 22:25:26 +0700

    cap nhat AuthContainer

--- src/components/AuthContainer.js
+++ src/components/AuthContainer.js
@@ -3,7 +3,11 @@ import Login from '../pages/Login';
 import Register from '../pages/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePages from '../pages/HomePages';
+import AddAriticle from '../pages/AddAriticle';
 import BlogDetails from './BlogDetails';
+import Profile from './Profile';
+import YourProfile from '../pages/YourProfile';
+import EditAritcle from '../pages/EditAritcle';
 
 const AuthContainer = () => {
     return (
@@ -12,7 +16,11 @@ const AuthContainer = () => {
                 <Route path="/" element={<HomePages />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/yourprofile" element={<YourProfile />} />
+                <Route path="/add" element={<AddAriticle />} />
+                <Route path="/profiles/:username" element={<Profile />} />
                 <Route path="/articles/:slug" element={<BlogDetails />} />
+                <Route path="/articlesedit/:slug" element={<EditAritcle />} />
             </Routes>
         </Router>
     );
